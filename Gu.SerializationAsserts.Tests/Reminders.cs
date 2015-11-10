2945dd44e60ed1e016bfe7ba64755384a7dabe26
Johan Larsson <johan_gavle1@hotmail.com>
2015-11-10 01:50:03 +0100

    Reminders.

--- Gu.SerializationAsserts.Tests/Reminders.cs
+++ Gu.SerializationAsserts.Tests/Reminders.cs
@@ -16,5 +16,17 @@
             Assert.Fail("elements");
             Assert.Fail("attributes");
         }
+
+        [Test]
+        public void BinaryAssert()
+        {
+            Assert.Fail("should throw");
+        }
+
+        [Test]
+        public void FieldsAssert()
+        {
+            Assert.Fail("should throw formatted message");
+        }
     }
 }
